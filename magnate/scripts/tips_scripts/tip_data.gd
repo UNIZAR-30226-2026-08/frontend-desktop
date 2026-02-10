355901d55c6e2712cdcb5bf5a0637166a8917181
Miguel Angel Luquín Guerrero <mluquinguerrero@gmail.com>
2026-02-10 19:01:27 +0100

    Tips scene and public lobby scene
    First version in Godot of these two scenes with their respected aux scenes

---
+++ magnate/scripts/tips_scripts/tip_data.gd
@@ -0,0 +1,5 @@
+class_name TipData extends Resource
+
+@export var number: String = "Number del Tip"
+@export var title: String = "Título del Tip"
+@export_multiline var description: String = "Aquí va el texto..."
