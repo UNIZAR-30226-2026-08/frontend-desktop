ce5ceefaf2a017dc76f80ffbc800650a5f720298
Nicolás de Rivas Morillo <843740@unizar.es>
2026-02-28 16:56:53 +0100

    Minor: Created a node as group for tiles of the board

--- magnate/scripts/board/board.gd
+++ magnate/scripts/board/board.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 @onready var camera_system: MagnateCameraSystem = $CameraSystem
+@onready var tiles: Node2D = $Tiles
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	BoardSpawner.spawn_board(self)
+	BoardSpawner.spawn_board(tiles)
 	camera_system.init_camera_system(self)
