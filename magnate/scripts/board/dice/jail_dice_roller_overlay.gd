89110db87da572ad39a9a1943ba01ea6d48df957
Miguel Royo <miguelroyo018@gmail.com>
2026-04-02 04:40:26 +0200

    feat: jail dice, bus dice, rigged dice
    Closes #87

--- magnate/scripts/board/dice/jail_dice_roller_overlay.gd
+++ magnate/scripts/board/dice/jail_dice_roller_overlay.gd
@@ -1,9 +1,9 @@
 extends Control
-class_name DiceRollerOverlay
+class_name JailDiceRollerOverlay
 
 signal roll_finished(total_value: int)
 
-@onready var dice_roller_3d: DiceRoller = $DiceRoller
+@onready var jail_dice_roller_3d: DiceRoller = $DiceRoller
 
 # NUEVA VARIABLE: Controla si ya se ha hecho una tirada
 # TODO: Quizá meter esto en un model para el juego
@@ -11,12 +11,12 @@ var has_rolled: bool = false
 
 func _ready() -> void:
 	mouse_filter = Control.MOUSE_FILTER_STOP
-	if dice_roller_3d:
-		dice_roller_3d.roll_finnished.connect(_on_3d_roll_finished)
+	if jail_dice_roller_3d:
+		jail_dice_roller_3d.roll_finnished.connect(_on_3d_roll_finished)
 
 func _gui_input(event: InputEvent) -> void:
 	# Si ya hemos tirado, o los dados están rodando, IGNORAMOS el click
-	if has_rolled or not dice_roller_3d or dice_roller_3d.rolling or not dice_roller_3d.interactive:
+	if has_rolled or not jail_dice_roller_3d or jail_dice_roller_3d.rolling or not jail_dice_roller_3d.interactive:
 		return
 		
 	if event is InputEventMouseButton:
@@ -25,16 +25,16 @@ func _gui_input(event: InputEvent) -> void:
 			var audio = AudioResource.from_type(Globals.AUDIO_DICE_ROLL, AudioResource.AudioResourceType.SFX)
 			AudioSystem.play_audio(audio)
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			dice_roller_3d.prepare()
+			jail_dice_roller_3d.prepare()
 		elif not event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			roll_the_dice()
 		elif event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
-			dice_roller_3d.quick_roll()
+			jail_dice_roller_3d.quick_roll()
 			has_rolled = true # Bloqueamos también si usa el botón derecho
 
-func roll_the_dice(forced_values: Array[int] = [1, 3, 6]) -> void:
+func roll_the_dice(forced_values: Array[int] = [1, 3]) -> void:
 	show()
-	dice_roller_3d.roll(forced_values)
+	jail_dice_roller_3d.roll(forced_values)
 	# Marcamos que ya se han tirado para no permitir más clicks
 	has_rolled = true 
 
