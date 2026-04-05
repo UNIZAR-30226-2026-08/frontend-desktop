ee3bb47f6b279a73d4e70508453dfd7d9045c8ac
Nicolás de Rivas Morillo <843740@unizar.es>
2026-04-05 17:04:28 +0200

    minor fix to format in Utils.debug

--- magnate/scripts/autoload/utils.gd
+++ magnate/scripts/autoload/utils.gd
@@ -4,7 +4,7 @@ extends RefCounted
 static func debug(msg: String) -> void:
 	if Globals.BUILD_TYPE == Globals.BuildType.DEV:
 		var time: Dictionary = Time.get_time_dict_from_system()
-		print("%d:%d:%d - LOG: " % [time.hour, time.minute, time.second] + msg)
+		print("%02d:%02d:%02d - LOG: " % [time.hour, time.minute, time.second] + msg)
 
 
 static func to_currency_text(value: int) -> String:
